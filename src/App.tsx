4399a85df5e8ea4fa7a2ff2c1fd56f76125c6397
Jonas Schäfer <schaefer-jonas@t-online.de>
2022-07-15 09:12:52 +0200

    articleobjekt

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-import type { Component } from 'solid-js';
+import { Component, createResource, For } from 'solid-js';
 
 import logo from './logo.svg';
 import styles from './App.module.css';
 
+const articles = [
+  { type: "Biete", title: "Nissan Skyline GTR R-34", img: "angebot.jpg", text: "Dieser wagen der Marke Nissan hat 280PS und kann maximal 180KM/h schnell fahren. Dieses Modell ist erst 75.068KM weit gefahren und wurde 2001 gebaut.\nUrsprünglich hat dieses Auto ca. 100.000 Geld gekostet." },
+  { type: "Suche", title:"Fußball", img: "Fussball.webp", text:"Ein Fußball im guten zustand"}
+];
+const loadArticles = () => new Promise<typeof articles>((resolve) => {
+  setTimeout(() => resolve(articles), 500);
+});
+
 const App: Component = () => {
+  const [articleData] = createResource(loadArticles);
   return (
     <div class={styles.App}>
       <header class={styles.header}>
@@ -15,24 +24,17 @@ const App: Component = () => {
           Herzlich Willkommen zum Tauschportal der CBG-Wörth!
           Dieses nicht kommerzielles Angebot richtet sich alleine an die Schülerinnen und Schüler der CBG-Wörth.
         </p>
+
+        <For each={articleData()} fallback={"Loading..."}>
+          {(article) => (<article>
+            <h2>{article.type}: {article.title}</h2>
+            <img src={`src/assets/${article.img}`} />
+            <p>
+              {article.text}
+            </p> 
+          </article>)}
+        </For>
         
-        <article>
-          <h2>
-            Biete: Nissan Skyline GTR R-34
-          </h2>
-          <img src="src/assets/angebot.jpg"/>
-          <p>
-            Dieser wagen der Marke Nissan hat 280PS und kann maximal 180KM/h schnell fahren. Dieses Modell ist erst 75.068KM weit gefahren und wurde 2001 gebaut.
-            Ursprünglich hat dieses Auto ca. 100.000 Geld gekostet.
-          </p>
-        </article>
-        <article>
-        <h2>Suche: Fußball</h2>
-        <img src="src/assets/Fussball.webp" />
-        <p>
-          Ein Fußball im guten zustand
-        </p>
-        </article>
         <section class={styles.form}>
           <input type="radio" name="art" value="Biete" id="biete" checked />
           <label for="biete">Biete</label>
